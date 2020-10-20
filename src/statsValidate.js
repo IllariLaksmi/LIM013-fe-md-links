9f85542c43cc19fb80cd8fe548a3b08bce3e30d2
Illari Alvarez-Gil <friedapples824@gmail.com>
2020-10-20 11:17:13 -0500

    Avances stats validate

--- src/statsValidate.js
+++ src/statsValidate.js
@@ -1,13 +1,7 @@
 //Funcion --stats --validate
-import {validate} from './validate.js'
+const fetch = require("node-fetch");
+import {mdlinksMethods} from './index.js'
 import {stats} from './stats.js'
 export const statsValidate = (links => {
-    let validatedLinks = validate(links);
-    let broken = 0;
-    validatedLinks.forEach(link => {
-        if((response.status >= 400  ) && (response.status <= 599)){
-            broken++;
-        }    
-    })
-    return console.log("Broken: " + broken);
+
     });
